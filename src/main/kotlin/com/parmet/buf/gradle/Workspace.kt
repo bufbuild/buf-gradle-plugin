57436eb70ad15709619c4b52f8919dbc03d8a916
Andrew Parmet <andrew@parmet.com>
2021-10-04 00:31:22 -0400

    rework protobuf source directory discovery

--- src/main/kotlin/com/parmet/buf/gradle/Workspace.kt
+++ src/main/kotlin/com/parmet/buf/gradle/Workspace.kt
@@ -2,58 +2,84 @@ package com.parmet.buf.gradle
 
 import java.io.File
 import java.nio.file.Files
-import java.nio.file.Paths
+import java.nio.file.Path
 import org.gradle.api.Project
+import org.gradle.api.file.SourceDirectorySet
+import org.gradle.api.tasks.SourceSetContainer
+import org.gradle.kotlin.dsl.get
+import org.gradle.kotlin.dsl.the
 
 const val CREATE_SYM_LINKS_TO_MODULES_TASK_NAME = "createSymLinksToModules"
 const val WRITE_WORKSPACE_YAML_TASK_NAME = "writeWorkspaceYaml"
 
-private const val SRC_MAIN_PROTO = "src/main/proto"
+private const val EXTRACT_INCLUDE_PROTO_TASK_NAME = "extractIncludeProto"
 private const val BUILD_EXTRACTED_INCLUDE_PROTOS_MAIN = "build/extracted-include-protos/main"
+
+private const val EXTRACT_PROTO_TASK_NAME = "extractProto"
+private const val BUILD_EXTRACTED_PROTOS_MAIN = "build/extracted-protos/main"
 
 internal fun Project.configureCreateSymLinksToModules() {
     tasks.register(CREATE_SYM_LINKS_TO_MODULES_TASK_NAME) {
         dependsOn(EXTRACT_INCLUDE_PROTO_TASK_NAME)
+        dependsOn(EXTRACT_PROTO_TASK_NAME)
+
         outputs.dir(bufbuildDir)
 
         doLast {
             File(bufbuildDir).mkdirs()
-            createSymLink(SRC_MAIN_PROTO)
-            createSymLink(BUILD_EXTRACTED_INCLUDE_PROTOS_MAIN)
+            protoDirs().forEach { createSymLink(it) }
         }
     }
 }
 
-private fun Project.createSymLink(protoDir: String) {
+private fun Project.createSymLink(protoDir: Path) {
     val symLinkFile = File(bufbuildDir, mangle(protoDir))
-    if (anyProtos(protoDir) && !symLinkFile.exists()) {
+    if (!symLinkFile.exists()) {
         logger.info("Creating symlink for $protoDir at $symLinkFile")
-        Files.createSymbolicLink(symLinkFile.toPath(), Paths.get(bufbuildDir).relativize(file(protoDir).toPath()))
+        Files.createSymbolicLink(symLinkFile.toPath(), Path.of(bufbuildDir).relativize(file(protoDir).toPath()))
     }
 }
-
-private fun mangle(name: String) =
-    name.replace("-", "--").replace(File.separator, "-")
-
-private fun Project.anyProtos(path: String) =
-    file(path).walkTopDown().any { it.extension == "proto" }
 
 internal fun Project.configureWriteWorkspaceYaml() {
     tasks.register(WRITE_WORKSPACE_YAML_TASK_NAME) {
         outputs.dir(bufbuildDir)
         doLast {
             File(bufbuildDir).mkdirs()
-            File("$bufbuildDir/buf.work.yaml").writeText(
+            val bufWork =
                 """
-                    version: v1
-                    directories:
-                      ${entry(SRC_MAIN_PROTO)}
-                      ${entry(BUILD_EXTRACTED_INCLUDE_PROTOS_MAIN)}
-                """.trimIndent()
-            )
+                    |version: v1
+                    |directories:
+                    ${workspaceSymLinkEntries()}
+                """.trimMargin()
+
+            logger.info("Writing generated buf.work.yaml: \n$bufWork")
+
+            File("$bufbuildDir/buf.work.yaml").writeText(bufWork)
         }
     }
 }
 
-private fun Project.entry(path: String) =
-    if (anyProtos(path)) "- ${mangle(path)}" else ""
+private fun Project.workspaceSymLinkEntries() =
+    protoDirs().joinToString("\n") { "|  - ${mangle(it)}" }
+
+private fun Project.protoDirs(): List<Path> =
+    potentialProtoDirs().filter { anyProtos(it) }
+
+private fun Project.potentialProtoDirs() =
+    the<SourceSetContainer>()["main"]
+        .extensions
+        .getByName("proto")
+        .let { it as SourceDirectorySet }
+        .let { sourceDirSet ->
+            sourceDirSet.srcDirs.map { projectDir.toPath().relativize(it.toPath()) }
+        } +
+            listOf(
+                BUILD_EXTRACTED_INCLUDE_PROTOS_MAIN,
+                BUILD_EXTRACTED_PROTOS_MAIN
+            ).map(Path::of)
+
+private fun Project.anyProtos(path: Path) =
+    file(path).walkTopDown().any { it.extension == "proto" }
+
+private fun mangle(name: Path) =
+    name.toString().replace("-", "--").replace(File.separator, "-")
