d50c5744e4282558c164c09802d8799c0e8529fb
Andrew Parmet <andrewparmet@users.noreply.github.com>
2022-07-06 17:42:06 -0400

    upgrade buf to 1.6.0

--- src/main/kotlin/com/parmet/buf/gradle/BufExtension.kt
+++ src/main/kotlin/com/parmet/buf/gradle/BufExtension.kt
@@ -50,7 +50,7 @@ open class BufExtension {
     /**
      * Specify the version of Buf.
      */
-    var toolVersion: String = "1.5.0"
+    var toolVersion: String = "1.6.0"
 
     internal var imageArtifactDetails: ArtifactDetails? = null
 
