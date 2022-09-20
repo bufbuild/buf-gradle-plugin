1938cf43a41148a596786a9868aa22b0b7915429
Andrew Parmet <andrewparmet@users.noreply.github.com>
2022-09-20 10:02:59 -0400

    remove buildSrc (#91)

--- build.gradle.kts
+++ build.gradle.kts
@@ -15,6 +15,10 @@
 
 import com.gradle.publish.PublishTask.GRADLE_PUBLISH_KEY
 import com.gradle.publish.PublishTask.GRADLE_PUBLISH_SECRET
+import com.vanniktech.maven.publish.JavadocJar
+import com.vanniktech.maven.publish.KotlinJvm
+import com.vanniktech.maven.publish.MavenPublishBaseExtension
+import com.vanniktech.maven.publish.SonatypeHost
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 repositories {
@@ -24,10 +28,11 @@ repositories {
 plugins {
     `java-gradle-plugin`
     `kotlin-dsl`
-    kotlin("jvm")
+    alias(libs.plugins.kotlin)
     alias(libs.plugins.pluginPublish)
     alias(libs.plugins.spotless)
     alias(libs.plugins.animalsniffer)
+    alias(libs.plugins.publishing)
 }
 
 allprojects {
@@ -44,7 +49,24 @@ allprojects {
 }
 
 group = "com.parmet"
-configurePublishing()
+
+dependencies {
+    signature(libs.java8Signature) { artifact { type = "signature" } }
+
+    testImplementation(libs.junit)
+    testImplementation(libs.truth)
+}
+
+configure<JavaPluginExtension> {
+    sourceCompatibility = targetJavaVersion
+    targetCompatibility = targetJavaVersion
+}
+
+object ProjectInfo {
+    const val name = "Buf Gradle Plugin"
+    const val url = "https://github.com/andrewparmet/buf-gradle-plugin"
+    const val description = "Buf plugin for Gradle"
+}
 
 gradlePlugin {
     isAutomatedPublishing = false
@@ -60,9 +82,6 @@ gradlePlugin {
 }
 
 pluginBundle {
-    mavenCoordinates {
-        group = project.group.toString()
-    }
     website = ProjectInfo.url
     vcsUrl = ProjectInfo.url
     description = ProjectInfo.description
@@ -91,14 +110,42 @@ tasks {
     }
 }
 
-dependencies {
-    signature(libs.java8Signature) { artifact { type = "signature" } }
+if (isRelease()) {
+    apply(plugin = "signing")
 
-    testImplementation(libs.junit)
-    testImplementation(libs.truth)
+    configure<SigningExtension> {
+        useInMemoryPgpKeys(
+            System.getenv("PGP_KEY")?.replace('$', '\n'),
+            System.getenv("PGP_PASSWORD")
+        )
+
+        sign(the<PublishingExtension>().publications)
+    }
 }
 
-configure<JavaPluginExtension> {
-    sourceCompatibility = targetJavaVersion
-    targetCompatibility = targetJavaVersion
+configure<MavenPublishBaseExtension> {
+    configure(KotlinJvm(JavadocJar.Empty()))
+    publishToMavenCentral(SonatypeHost.DEFAULT)
+    pom {
+        name.set(ProjectInfo.name)
+        description.set(ProjectInfo.description)
+        url.set(ProjectInfo.url)
+        scm { url.set(ProjectInfo.url) }
+        licenses {
+            license {
+                name.set("The Apache License, Version 2.0")
+                url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+            }
+        }
+        developers {
+            developer {
+                id.set("Andrew Parmet")
+                name.set("Andrew Parmet")
+                email.set("andrew@parmet.com")
+            }
+        }
+    }
 }
+
+fun Project.isRelease() =
+    !version.toString().endsWith("-SNAPSHOT")
