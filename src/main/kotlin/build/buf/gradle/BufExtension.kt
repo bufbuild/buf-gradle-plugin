28a1132430fd46b2e31ba143592289e3feab7b1a
Philip K. Warren <pwarren@buf.build>
2023-10-18 13:42:43 -0500

    Bump the Buf CLI version to the latest

--- src/main/kotlin/build/buf/gradle/BufExtension.kt
+++ src/main/kotlin/build/buf/gradle/BufExtension.kt
@@ -49,7 +49,7 @@ open class BufExtension {
     /**
      * Specify the version of Buf.
      */
-    var toolVersion = "1.17.0"
+    var toolVersion = "1.27.1"
 
     internal var imageArtifactDetails: ArtifactDetails? = null
 
