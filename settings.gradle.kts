36494d7e83e06903a750719f6746a2dc0f1c4004
Andrew Parmet <andrew@parmet.com>
2024-01-18 19:55:40 -0500

    foojay

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ buildscript { repositories { gradlePluginPortal() } }
 
 plugins {
     id("io.alcide.gradle-semantic-build-versioning") version "4.2.2"
+    id("org.gradle.toolchains.foojay-resolver-convention") version "0.4.0"
 }
 
 rootProject.name = "buf-gradle-plugin"
