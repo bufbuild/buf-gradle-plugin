c5a0994eab3148bc834fefda7acf8d49ebf7ad89
Marcel Šebek <sebek64@post.cz>
2024-07-03 16:42:46 +0200

    Remove no longer needed createsOutput call - outputs now created automatically by gradle

--- src/main/kotlin/build/buf/gradle/ProtobufGradlePluginSupport.kt
+++ src/main/kotlin/build/buf/gradle/ProtobufGradlePluginSupport.kt
@@ -124,7 +124,6 @@ private fun Task.workspaceCommonConfig() {
             .tasks
             .matching { it::class.java.name == "com.google.protobuf.gradle.ProtobufExtract_Decorated" },
     )
-    createsOutput()
 }
 
 private fun WriteWorkspaceYamlTask.workspaceSymLinkEntries() =
