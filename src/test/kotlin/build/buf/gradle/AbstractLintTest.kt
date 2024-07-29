9f5e9f4ca12d87e408c9d7936ff8c558a7ad2935
Dan Rice <drice@buf.build>
2024-07-29 12:47:20 -0400

    Add v2 buf.yaml LintWithWorkspaceTest test cases

--- src/test/kotlin/build/buf/gradle/AbstractLintTest.kt
+++ src/test/kotlin/build/buf/gradle/AbstractLintTest.kt
@@ -51,7 +51,7 @@ abstract class AbstractLintTest : LintTestUtilities, AbstractBufIntegrationTest(
         assertSuccess()
     }
 
-    private fun assertBadEnumSuffix() {
+    protected fun assertBadEnumSuffix() {
         val result = checkRunner().buildAndFail()
         assertThat(result.task(":$BUF_LINT_TASK_NAME")?.outcome).isEqualTo(FAILED)
         assertThat(result.output).contains(
