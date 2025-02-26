444e7d34fa5ad7bf2661364d7b21958aff6a3152
Andrew Parmet <andrew@parmet.com>
2025-02-26 12:47:57 -0500

    always

--- src/test/kotlin/build/buf/gradle/AbstractBufIntegrationTest.kt
+++ src/test/kotlin/build/buf/gradle/AbstractBufIntegrationTest.kt
@@ -26,7 +26,7 @@ import java.nio.file.Paths
 import java.util.Locale
 
 abstract class AbstractBufIntegrationTest : IntegrationTest {
-    @TempDir(cleanup = CleanupMode.NEVER)
+    @TempDir(cleanup = CleanupMode.ALWAYS)
     lateinit var projectDir: File
 
     val buildFile
